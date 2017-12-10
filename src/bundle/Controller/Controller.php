53c728cfbc1a700535460a1971e54deb1761178e
Emmanuel DROUET <emmanuel.drouet@gmail.com>
2017-12-11 00:17:24 +0100

    fix controller access check

---
+++ src/bundle/Controller/Controller.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Edgar\EzUICronBundle\Controller;
+
+use Edgar\EzUICronBundle\Service\EzCronService;
+use eZ\Publish\Core\Base\Exceptions\UnauthorizedException;
+use EzSystems\EzPlatformAdminUiBundle\Controller\Controller as BaseController;
+use eZ\Publish\API\Repository\Repository;
+
+class Controller extends BaseController
+{
+    /** @var EzCronService $cronService cron service */
+    protected $cronService;
+
+    /** @var Repository $repository */
+    protected $repository;
+
+    public function __construct(
+        EzCronService $cronService,
+        Repository $repository
+    ) {
+        $this->cronService = $cronService;
+        $this->repository = $repository;
+    }
+
+    public function performAccessCheck()
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+        if ($this->repository->hasAccess('cron', 'list') !== true) {
+            throw new UnauthorizedException('cron', 'list');
+        }
+    }
+}
--- src/bundle/Controller/CronController.php
+++ src/bundle/Controller/CronController.php
@@ -2,29 +2,11 @@
 
 namespace Edgar\EzUICronBundle\Controller;
 
-use Edgar\EzUICronBundle\Service\EzCronService;
-use eZ\Publish\API\Repository\Repository;
-use eZ\Publish\Core\Base\Exceptions\UnauthorizedException;
-use EzSystems\EzPlatformAdminUiBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class CronController extends Controller
 {
-    /** @var EzCronService $cronService cron service */
-    protected $cronService;
-
-    /** @var Repository $repository */
-    protected $repository;
-
-    public function __construct(
-        EzCronService $cronService,
-        Repository $repository
-    ) {
-        $this->cronService = $cronService;
-        $this->repository = $repository;
-    }
-
     public function menuAction(Request $request): Response
     {
         $crons = $this->cronService->getCrons();
@@ -32,13 +14,5 @@ class CronController extends Controller
         return $this->render('@EdgarEzUICron/cron/view.html.twig', [
             'crons' => $crons,
         ]);
-    }
-
-    public function performAccessCheck()
-    {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
-        if ($this->repository->hasAccess('cron', 'list') !== true) {
-            throw new UnauthorizedException('cron', 'list');
-        }
     }
 }
